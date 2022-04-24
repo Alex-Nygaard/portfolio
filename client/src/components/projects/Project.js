5efcacf6e030118362cba3f2a0896d3c1d9f6c45
Alexander Nygård <A.K.Nygard@student.tudelft.nl>
2022-04-24 14:30:57 +0200

    Changed project section

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 
+import ImagePlaceholder from "./projectImages/placeholder.jpeg";
+
 const Project = (props) => {
+    console.log(props.img);
     return (
         <div className="project-holder">
-            <img className="project-img" src={props.img} alt="" />
+            <img
+                className="project-img"
+                src={props.img !== undefined ? props.img : ImagePlaceholder}
+                alt=""
+            />
             <h2>{props.title}</h2>
             <h4>{props.type}</h4>
             <p className="gitlink">
--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./projects.css";
 import Project from "./Project";
 
+import QuizzzImage from "./projectImages/quizzz.png";
+import MarketplaceImage from "./projectImages/marketplace.png";
+import ISAKNOWImage from "./projectImages/isaknow.png";
+
 const Projects = () => {
     return (
         <section id="projects">
@@ -10,11 +14,28 @@ const Projects = () => {
                 <Project
                     title="Quizzzz app"
                     type="Object Oriented Programming Project"
+                    img={QuizzzImage}
                 />
-                <Project title="Testing1" />
-                <Project title="Testing1" />
-                <Project title="Testing1" />
-                <Project title="Testing1" />
+                <Project
+                    title="Marketplace platform"
+                    type="Computer Science class project"
+                    img={MarketplaceImage}
+                />
+                <Project
+                    title="ISAKNOW"
+                    type="Creativity, Activity and Service Project"
+                    img={ISAKNOWImage}
+                />
+                <Project
+                    title="A comparison study on detection of Deepfakes using ANNs"
+                    type="Deep learning research project"
+                />
+                <Project title="Memory" type="Web Technology class project" />
+                <Project
+                    title="Book inventory"
+                    type="Computer Science class project"
+                />
+                <Project title="Portfolio" type="This project" />
             </div>
         </section>
     );

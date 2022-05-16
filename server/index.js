aa06a6c36ec761ca92e7d189d9e3b1f6f928024f
Alexander Nygård <A.K.Nygard@student.tudelft.nl>
2022-05-17 00:08:10 +0200

    Added favicon

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
